155e74fa43c71f9ce2eb1bf0772ee8661d951334
hunterr84 <55663047+hunterr84@users.noreply.github.com>
2020-08-24 15:17:47 +0300

    Update query.py

--- query.py
+++ query.py
@@ -36,22 +36,3 @@ def get_network_status(substrate):
     block_timestate = network.get_runtime_block(block_hash)['block']['extrinsics'][0]['params'][0]['value']
 
     return parachain_name, version, token, peers_count, block_hash, block_number, block_timestate
-
-if  __name__ == '__main__':
-    network = si.SubstrateInterface(url="wss://vladivostok-rpc.robonomics.network")
-    #version = si.SubstrateInterface(url="wss://vladivostok-rpc.robonomics.network").rpc_request(
-    #    method='chain_getBlock',
-   #     params=[],
-   #     result_handler=None
-   #)
-    block_hash = network.get_chain_finalised_head()
-    print(block_hash)
-    block_number = network.get_runtime_block(block_hash)['block']['extrinsics'][0]['params'][0]['value']
-    print(block_number)
-    timestamp = 1598264150.041
-    value = datetime.datetime.fromtimestamp(timestamp)
-    print(value.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
-
-    #{'block': {'extrinsics': [{'valueRaw': '040200', 'extrinsic_length': 10, 'version_info': '04', 'call_code': '0200',
-    #                           'call_function': 'set', 'call_module': 'timestamp',
-     #                          'params': [{'name': 'now', 'type': 'Compact<Moment>',
