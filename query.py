4a6ab1e53e7b4c18d0c4d3a85cf85157268e01f2
hunterr84 <ura-01@yandex.ru>
2020-08-24 14:38:13 +0200

    add except

--- query.py
+++ query.py
@@ -33,6 +33,8 @@ def get_network_status(substrate):
 
     block_hash = network.get_chain_head()
     block_number = network.get_block_number(block_hash)
-    block_timestate = network.get_runtime_block(block_hash)['block']['extrinsics'][0]['params'][0]['value']
-
+    try:
+        block_timestate = network.get_runtime_block(block_hash)['block']['extrinsics'][0]['params'][0]['value']
+    except Exception:
+        block_timestate = 'none'
     return parachain_name, version, token, peers_count, block_hash, block_number, block_timestate
